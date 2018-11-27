088f3f08d60133ea6f5b1ae5fe0acd04534106ab
yekai <gaoye@tokenpai.com>
2018-11-27 10:21:34 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"go-finepoints/configs"
+	"go-finepoints/routes"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+var EchoObj *echo.Echo //echo框架对象全局定义
+
+func main() {
+
+	fmt.Printf("get config %v ,%v\n", configs.Config.Common.Port, configs.Config.Db.Connstr)
+	EchoObj = echo.New()             //创建echo对象
+	EchoObj.Use(middleware.Logger()) //安装日志中间件
+	EchoObj.Use(middleware.Recover())
+	EchoObj.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 5,
+	}))
+	EchoObj.GET("/ping", routes.PingHandler)                        //路由测试函数
+	EchoObj.Logger.Fatal(EchoObj.Start(configs.Config.Common.Port)) //启动服务
+}
